112502bbffd8ce099930f763b3279cad05c7bf4e
Muhammad Owais <mowais@folio3.com>
2025-05-11 21:02:20 +0500

    update

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,7 +1,7 @@
 // src/App.jsx
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import './assets/styles/App.css';
+import './App.css';
 
 // Pages
 import Home from './pages/Home';
--- Frontend/src/index.jsx
+++ Frontend/src/index.jsx
@@ -11,20 +11,3 @@ root.render(
   </React.StrictMode>
 );
 
-// src/components/common/PrivateRoute.jsx
-import React from 'react';
-import { Navigate } from 'react-router-dom';
-import { useContext } from 'react';
-import AuthContext from '../../context/auth/authContext';
-import Loading from './Loading';
-
-const PrivateRoute = ({ element }) => {
-  const authContext = useContext(AuthContext);
-  const { isAuthenticated, loading } = authContext;
-
-  if (loading) return <Loading />;
-  
-  return isAuthenticated ? element : <Navigate to="/login" />;
-};
-
-export default PrivateRoute;
